6096523fef5456c2a98a66fa93fc3f6d7d5d24ff
Rifo <muhammadrifo58@gmail.com>
2025-11-18 22:29:50 +0700

    tugas W08

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// Perubahan: Mengimpor trait SoftDeletes
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
+    // Perubahan: Menggunakan trait SoftDeletes
+    use SoftDeletes;
+    
     protected $fillable = ['name', 'email', 'message'];
 }
